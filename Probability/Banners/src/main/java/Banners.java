ae6c3f6ff1f3b16b48ad4ef1c6caea3b81f5916b
aparfenov_user <aparfenov_user>
2018-01-31 18:01:24 +0300

    <= fix

--- Probability/Banners/src/main/java/Banners.java
+++ Probability/Banners/src/main/java/Banners.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.Map;
 import java.util.Random;
 import java.util.TreeMap;
@@ -8,12 +9,12 @@ import java.util.TreeMap;
 public class Banners {
     private static TreeMap<Integer, String> elements;
     public static void main(String [] args) {
-        int n = 1000000;
+        int n = 10000000;
         init();
         Random r = new Random();
         TreeMap<String, Integer> counts = new TreeMap<String, Integer>();
+        Integer maxWeight = elements.lastKey();
         for (int i = 0; i < n; i++) {
-            Integer maxWeight = elements.lastKey();
             Integer rnd = r.nextInt(maxWeight);
             String bannerName = getElement(rnd);
 
@@ -26,24 +27,29 @@ public class Banners {
         }
 
         for (Map.Entry nextEntry: counts.entrySet()) {
-            System.out.println(nextEntry.getKey() + "=" + nextEntry.getValue());
+            DecimalFormat myFormatter = new DecimalFormat("0.0000000");
+            System.out.println(nextEntry.getKey() + "=" +
+                    nextEntry.getValue() + "["
+                    + myFormatter.format((double)counts.lastEntry().getValue()/(double)((Integer)nextEntry.getValue()).intValue())
+                    + "]"
+            );
         }
     }
 
     private static void init() {
         elements = new TreeMap<Integer, String>();
-        addElement(elements, 1, "Banner1");
-        addElement(elements,2, "Banner2");
-        addElement(elements,3, "Banner3");
-        addElement(elements,4, "Banner4");
-        addElement(elements,5, "Banner5");
-        addElement(elements,6, "Banner6");
-        addElement(elements,7, "Banner7");
-        addElement(elements,8, "Banner8");
-        addElement(elements,9, "Banner9");
+        addElement(1, "Banner1");
+        addElement(2, "Banner2");
+        addElement(3, "Banner3");
+        addElement(4, "Banner4");
+        addElement(5, "Banner5");
+        addElement(6, "Banner6");
+        addElement(7, "Banner7");
+        addElement(8, "Banner8");
+        addElement(9, "Banner9");
     }
 
-    private static void addElement(TreeMap tm, Integer weight, String bannerName) {
+    private static void addElement(Integer weight, String bannerName) {
         int maxWeight = 0;
         if (elements.size() > 0) {
             maxWeight = elements.lastKey();
@@ -53,6 +59,14 @@ public class Banners {
 
     private static String getElement(Integer rnd) {
         Map.Entry<Integer, String> ceilingEntry = elements.ceilingEntry(rnd);
-        return ceilingEntry.getValue();
+        if (ceilingEntry == null) {
+            return elements.firstEntry().getValue();
+        }
+        //avoiding equal keys, only rnd less then "weight" key
+        if (ceilingEntry.getKey().equals(rnd)) {
+            return elements.higherEntry(ceilingEntry.getKey()).getValue();
+        } else {
+            return ceilingEntry.getValue();
+        }
     }
 }
